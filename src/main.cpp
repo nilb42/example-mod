17a77646b58cc8b777a835ae2370662796fb83bd
nilb42 <nilboryatinsky@gmail.com>
2026-03-26 20:59:34 +0100

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,20 +9,15 @@ bool g_isModActive = true;
 class $modify(MyUILayer, UILayer) {
     void keyDown(cocos2d::enumKeyCodes key) {
         UILayer::keyDown(key);
-        // Проверка нажатия клавиши [
+        // Бесшумное переключение мода по кнопке [
         if (key == cocos2d::enumKeyCodes::KEY_LeftBracket) {
             g_isModActive = !g_isModActive;
-            if (g_isModActive) {
-                FLAlertLayer::create("Smooth Spam", "Mod is ON", "OK")->show();
-            } else {
-                FLAlertLayer::create("Smooth Spam", "Mod is OFF", "OK")->show();
-            }
         }
     }
 };
 
 class $modify(MyPlayer, PlayerObject) {
-    // Просто чистый перехват прыжка, без сложной физики
+    // Пустой перехват прыжка для проверки сборки
     void pushButton(PlayerButton btn) {
         PlayerObject::pushButton(btn);
     }
